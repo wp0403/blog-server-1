36f400434e0ab82f01f5501c5cf172f341550eb0
wengpeng0403 <webwp0403@163.com>
2022-02-11 16:19:01 +0800

    fix:

---
+++ server.js
@@ -0,0 +1,8 @@
+/* eslint-disable strict */
+const egg = require('egg');
+
+const workers = Number(process.argv[2] || require('os').cpus().length);
+egg.startCluster({
+  workers,
+  baseDir: __dirname,
+});
