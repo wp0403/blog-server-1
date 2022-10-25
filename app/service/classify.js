ee7d82ebe3dd8fcc13565b337f08d2328f31a1cf
王鹏 <75709526+wp0403@users.noreply.github.com>
2022-10-25 16:03:41 +0800

    fix:

--- app/service/classify.js
+++ app/service/classify.js
@@ -4,7 +4,7 @@
  * @Author: 王鹏
  * @Date: 2021-08-13 10:05:07
  * @LastEditors: WangPeng
- * @LastEditTime: 2022-08-11 17:52:07
+ * @LastEditTime: 2022-10-25 16:02:15
  */
 'use strict';
 
@@ -12,26 +12,25 @@ const Service = require('egg').Service;
 
 class ClassifyService extends Service {
   async _getClassifyList(obj) {
-    const bowenListNum = await this.app.mysql.query('select count(*) from Bowen where classify_id=? and type in (?) and isDelete in (?)', [ obj.id, 1, 0 ]);
+    const bowenListNum = await this.app.mysql.query(
+      'select count(*) from Bowen where classify_id=? and type in (?) and isDelete in (?)',
+      [ obj.id, 1, 0 ]
+    );
 
-    const bowenList = await this.app.mysql.select('Bowen', {
-      where: { classify_id: obj.id, type: 1, isDelete: 0 },
-      columns: [
-        'id',
-        'time_str',
-        'last_edit_time',
-        'img',
-        'author',
-        'classify',
-        'classify_id',
-        'classify_sub',
-        'classify_sub_id',
-        'title',
-        'desc',
-      ],
-      limit: +obj.page_size,
-      offset: (obj.page - 1) * obj.page_size,
-    });
+    let sql =
+      'select a.*,json_object("id",b.uid,"name",b.name,"email",b.email) as userInfo from Bowen a left join admin b on a.author_id = b.uid where a.type in (?) and a.classify_id in ? and a.isDelete in (?)';
+
+    const content = [ 1, obj.id, 0 ];
+
+    // 开启分页
+    if (obj.page || obj.page_size) {
+      const current = obj.page; // 当前页码
+      const pageSize = obj.page_size; // 一页展示多少条数据
+      sql += ' limit ?,?';
+      content.push((current - 1) * pageSize, parseInt(pageSize));
+    }
+
+    const bowenList = await this.app.mysql.query(sql, content);
 
     return {
       data: bowenList,
@@ -44,7 +43,10 @@ class ClassifyService extends Service {
   }
 
   async _getClassifySubList(obj) {
-    const bowenListNum = await this.app.mysql.query('select count(*) from Bowen where classify_sub_id=? and type in (?) and isDelete in (?)', [ obj.id, 1, 0 ]);
+    const bowenListNum = await this.app.mysql.query(
+      'select count(*) from Bowen where classify_sub_id=? and type in (?) and isDelete in (?)',
+      [ obj.id, 1, 0 ]
+    );
 
     const bowenList = await this.app.mysql.select('Bowen', {
       where: { classify_sub_id: obj.id, type: 1, isDelete: 0 },
@@ -76,18 +78,21 @@ class ClassifyService extends Service {
   }
 
   async _getClassifyDetails(id) {
-    return await this.app.mysql.get('Bowen', { id, type: 1, isDelete: 0 });
+    const sql =
+      'select a.*,json_object("id",b.uid,"name",b.name) as userInfo from Bowen a left join admin b on a.author_id = b.uid where a.id = ? and type = ? and isDelete = ?';
+    const list = await this.app.mysql.query(sql, [ id, 1, 0 ]);
+    return list && list[0]
+      ? { ...list[0], userInfo: JSON.parse(list[0].userInfo) }
+      : {};
   }
 
   async _getClassifyDetailsFooter(id) {
-    const bowenListNum = await this.app.mysql.query('select count(*) from Bowen where type in (1) and isDelete in (0)');
+    const bowenListNum = await this.app.mysql.query(
+      'select count(*) from Bowen where type in (1) and isDelete in (0)'
+    );
     const bowenList = await this.app.mysql.select('Bowen', {
       where: { type: 1, isDelete: 0 },
-      columns: [
-        'id',
-        'img',
-        'title',
-      ],
+      columns: [ 'id', 'img', 'title' ],
     });
 
     const index = bowenList.findIndex(v => +v.id === +id);
@@ -99,7 +104,8 @@ class ClassifyService extends Service {
       },
       {
         name: 'next',
-        obj: index < bowenListNum[0]['count(*)'] - 1 ? bowenList[index + 1] : {},
+        obj:
+          index < bowenListNum[0]['count(*)'] - 1 ? bowenList[index + 1] : {},
       },
     ];
   }
