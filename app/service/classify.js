b3b4493cdb967c34d01adae8020f281f8d565327
王鹏 <75709526+wp0403@users.noreply.github.com>
2023-01-19 11:37:21 +0800

    Update classify.js

--- app/service/classify.js
+++ app/service/classify.js
@@ -4,7 +4,7 @@
  * @Author: 王鹏
  * @Date: 2021-08-13 10:05:07
  * @LastEditors: WangPeng
- * @LastEditTime: 2023-01-19 11:27:11
+ * @LastEditTime: 2023-01-19 11:37:13
  */
 'use strict';
 
@@ -38,7 +38,7 @@ class ClassifyService extends Service {
         page: obj.page,
         page_size: obj.page_size,
         total: bowenListNum[0]['count(*)'],
-        totalPage: Math.round(bowenListNum[0]['count(*)'] / obj.page_size),
+        totalPage: Math.ceil(bowenListNum[0]['count(*)'] / obj.page_size),
       },
     };
   }
@@ -70,7 +70,7 @@ class ClassifyService extends Service {
         page: obj.page,
         page_size: obj.page_size,
         total: bowenListNum[0]['count(*)'],
-        totalPage: Math.round(bowenListNum[0]['count(*)'] / obj.page_size),
+        totalPage: Math.ceil(bowenListNum[0]['count(*)'] / obj.page_size),
       },
     };
   }
